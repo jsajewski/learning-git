42c42c24952e7c24ef98c11513f5544db1991a84
Jakub Sajewski <kuba.sajewski@o2.pl>
2025-05-24 15:48:03 +0200

    Dodano main.py

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("Hello, world!") 
+
+zakupy = {
+    "piekarnia": ["chleb", "bułki", "pączek"],
+    "warzywniak": ["marchew", "seler", "rukola"]
+        }
+print("---Lista Zakupów---\n")
+
+sum_stuff = 0
+for shop, stuff in zakupy.items():
+    sum_stuff += len(stuff)
+    shop_name = shop.title()
+    produkty = ', '.join([item.title() for item in stuff])
+
+    if shop == "piekarnia":
+        print(f"Idę do {shop_name}, kupuje tu: {produkty}\n")
+    elif shop == "warzywniak":
+        print(f"Idę do {shop_name} i kupije tu: {produkty}\n")
+
+print(f"W sumie kupuję: {sum_stuff} produktów.")
